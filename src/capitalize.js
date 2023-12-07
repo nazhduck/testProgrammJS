45cb7229d97ab299740538c4be097f339bb769aa
Артём Тихонов <mishilen2002@gmail.com>
2023-12-07 11:21:02 +0300

    add assert in test

--- src/capitalize.js
+++ src/capitalize.js
@@ -3,7 +3,7 @@ const capitalize = (string) => {
     return "";
   }
   const firstChar = string[0].toUpperCase();
-  const restSubstring = string.slice(1).toLowerCase();
+  const restSubstring = string.slice(1);
   return `${firstChar}${restSubstring}`;
 };
 
--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,15 +1,10 @@
+import { strict as assert } from "node:assert";
+
 import capitalize from "../src/capitalize.js";
 
-if (capitalize("hello") !== "Hello") {
-  throw new Error("Функция работает неверно!");
-}
-
-if (capitalize("") !== "") {
-  throw new Error("Функция работает неверно!");
-}
-
-if (capitalize("heLlO") !== "Hello") {
-  throw new Error("Функция работает неверно!")
-}
+assert.equal(capitalize('hello!'), 'Hello!')
+assert.equal(capitalize(''), '')
+// assert.equal(capitalize('heLlO!'), 'Hello!')
+// assert.equal(capitalize(' hello!'), 'Hello!')
 
 console.log("Все тесты пройдены!");
